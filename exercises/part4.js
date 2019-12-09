3252958ceb4f5c67b0a31bd495235e1c5a556288
Svagtlys <45446348+Svagtlys@users.noreply.github.com>
2019-12-09 11:02:55 -0600

    Completed all exercises. Tiny issue when I forgot to save a file. Easy fix.

--- exercises/part4.js
+++ exercises/part4.js
@@ -1,41 +1,29 @@
-let kilometersToMars: number = 225000000;
-let kilometersToTheMoon: number = 384400;
-
-
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
 // The variables that are commented out will be moved into the Spacecraft class
 // let spaceCraft: string = "Space Shuttle";
 // let speedMph: number = 17500;
 // let milesPerKilometer: number = 0.621;
-
 // This function will also be moved into the Spacecraft class
 // function getDaysToLocation(kilometersAway: number): number {
 //   let milesAway: number = kilometersAway * milesPerKilometer;
 //   let hours: number = milesAway / speedMph;
 //   return hours / 24;
 // }
-
 // Define your Spacecraft class here:
-class Spacecraft{
-    milesPerKilometer: number = 0.621;
-    name: string;
-    speedMph: number;
-
-    constructor(name: string, speedMph: number){
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
         this.name = name;
         this.speedMph = speedMph;
     }
-
-    getDaysToLocation(kilometersAway: number): number{
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
         return ((kilometersAway * this.milesPerKilometer) / this.speedMph) / 24;
-    }
-
-}
-
-
+    };
+    return Spacecraft;
+}());
 // Create an instance of the class here:
-let spaceShuttle = new Spacecraft('Determination', 17500);
-
+var spaceShuttle = new Spacecraft('Determination', 17500);
 // Print two outputs - one for the trip to Mars and one for the trip to the moon.
-console.log(`${spaceShuttle.name} has ${spaceShuttle.getDaysToLocation(kilometersToMars)} days left until it reaches Mars.`);
-console.log(`${spaceShuttle.name} has ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days left until it reaches the Moon.`);
-
+console.log(spaceShuttle.name + " has " + spaceShuttle.getDaysToLocation(kilometersToMars) + " days left until it reaches Mars.");
+console.log(spaceShuttle.name + " has " + spaceShuttle.getDaysToLocation(kilometersToTheMoon) + " days left until it reaches the Moon.");
